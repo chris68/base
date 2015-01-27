3a5f38fe073b030ac35025d89a4c35036691d93c
chris68 <chris68@users.noreply.github.com>
2015-01-27 23:21:24 +1100

    Update pg_backup_rotated.sh
    User logger instead of echo

--- pg_backup_rotated.sh
+++ pg_backup_rotated.sh
@@ -62,7 +62,7 @@ function perform_backups()
 	SUFFIX=$1
 	FINAL_BACKUP_DIR=$BACKUP_DIR"`date +\%Y-\%m-\%d`$SUFFIX/"
  
-	echo "Making backup directory in $FINAL_BACKUP_DIR"
+	logger "pg_backup: Making backup directory in $FINAL_BACKUP_DIR"
  
 	if ! mkdir -p $FINAL_BACKUP_DIR; then
 		echo "Cannot create backup directory in $FINAL_BACKUP_DIR. Go and fix it!" 1>&2
@@ -93,7 +93,7 @@ function perform_backups()
 	        echo "Schema-only backup of $DATABASE"
  
 	        if ! pg_dump -Fp -s -h "$HOSTNAME" -U "$USERNAME" "$DATABASE" | gzip > $FINAL_BACKUP_DIR"$DATABASE"_SCHEMA.sql.gz.in_progress; then
-	                echo "[!!ERROR!!] Failed to backup database schema of $DATABASE" 1>&2
+	                logger -s "pg_backup [!!ERROR!!] Failed to backup database schema of $DATABASE" 1>&2
 	        else
 	                mv $FINAL_BACKUP_DIR"$DATABASE"_SCHEMA.sql.gz.in_progress $FINAL_BACKUP_DIR"$DATABASE"_SCHEMA.sql.gz
 	        fi
@@ -121,7 +121,7 @@ function perform_backups()
 			echo "Plain backup of $DATABASE"
  
 			if ! pg_dump -Fp -h "$HOSTNAME" -U "$USERNAME" "$DATABASE" | gzip > $FINAL_BACKUP_DIR"$DATABASE".sql.gz.in_progress; then
-				echo "[!!ERROR!!] Failed to produce plain backup database $DATABASE" 1>&2
+				logger -s "pg_backup  [!!ERROR!!] Failed to produce plain backup database $DATABASE" 1>&2
 			else
 				mv $FINAL_BACKUP_DIR"$DATABASE".sql.gz.in_progress $FINAL_BACKUP_DIR"$DATABASE".sql.gz
 			fi
@@ -132,7 +132,7 @@ function perform_backups()
 			echo "Custom backup of $DATABASE"
  
 			if ! pg_dump -Fc -h "$HOSTNAME" -U "$USERNAME" "$DATABASE" -f $FINAL_BACKUP_DIR"$DATABASE".custom.in_progress; then
-				echo "[!!ERROR!!] Failed to produce custom backup database $DATABASE"
+				logger -s "pg_backup [!!ERROR!!] Failed to produce custom backup database $DATABASE"
 			else
 				mv $FINAL_BACKUP_DIR"$DATABASE".custom.in_progress $FINAL_BACKUP_DIR"$DATABASE".custom
 			fi
